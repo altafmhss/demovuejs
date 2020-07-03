61c8a4c7d836118aeee1c7d2a0c5737deec9e20d
altafmhss <36732944+altafmhss@users.noreply.github.com>
2020-07-03 12:55:13 +0530

    Add files via upload

---
+++ src/components/home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>Items</h1>
+
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Item Name</td>
+          <td>Item Price</td>
+          <td>Actions</td>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="item in items" :key="item._id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.quantity }}</td>
+          <td>{{ item.price }}</td>
+          <td>
+            <router-link :to="{name: 'Edit', params: { id: item.id }}" class="btn btn-primary">Edit</router-link>
+          </td>
+          <td>
+            <button class="btn btn-danger" v-on:click="deleteItem(item.id)">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  created: function() {
+    this.fetchItems();
+  },
+  methods: {
+    fetchItems() {
+      let uri = "http://localhost:8013/products";
+      this.axios.get(uri).then(response => {
+        this.items = response.data;
+      });
+    },
+
+    deleteItem(id) {
+      this.axios.delete(`http://localhost:8013/delete/${id}`).then(() => {
+                 this.fetchItems();
+            });
+    }
+  }
+};
+</script>
